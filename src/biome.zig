4f292f4cd97d1a587151fac4483ac1787bf3d2a4
Martin <66840021+Mcrtin@users.noreply.github.com>
2026-04-24 11:15:31 +0200

    start working on rtree

--- src/biome.zig
+++ src/biome.zig
@@ -148,3 +148,173 @@ const LCG = struct {
         return best_pos;
     }
 };
+
+const MyRTree = RTree(TODO, 6);
+
+// protected long distance(long[] values) {
+//     long l = 0L;
+//
+//     for (int i = 0; i < 7; i++) {
+//         l += Mth.square(this.parameterSpace[i].distance(values[i]));
+//     }
+//
+//     return l;
+// }
+pub fn RTree(comptime T: type, comptime ParameterSpace: type, comptime ParameterInstance: type, comptime pspace_size: usize, comptime metric: fn (node: Node, searchedValues: []const i64) i64, comptime comparator: fn (pspace: ParameterSpace, pInstance: ParameterInstance) usize, comptime children_per_node: usize) type {
+    return struct {
+        root: Node,
+        last: ?T,
+
+        pub fn create(nodes: anytype) @This() {
+            std.debug.assert(nodes.len > 0);
+            // const list = nodes.stream().map(node -> new Climate.RTree.Leaf<>(node.getFirst(), node.getSecond()))
+            //         .collect(Collectors.toCollection(ArrayList::new));
+            // return new Climate.RTree<>(build(size, list));
+        }
+
+        // private static <T> Climate.RTree.Node<T> build(int paramSpaceSize,
+        //         List<? extends Climate.RTree.Node<T>> children) {
+        //     if (children.isEmpty()) {
+        //         throw new IllegalStateException("Need at least one child to build a node");
+        //     } else if (children.size() == 1) {
+        //         return (Climate.RTree.Node<T>) children.get(0);
+        //     } else if (children.size() <= 6) {
+        //         children.sort(Comparator.comparingLong(child -> {
+        //             long l2 = 0L;
+        //
+        //             for (int i2 = 0; i2 < paramSpaceSize; i2++) {
+        //                 Climate.Parameter parameter = child.parameterSpace[i2];
+        //                 l2 += Math.abs((parameter.min() + parameter.max()) / 2L);
+        //             }
+        //
+        //             return l2;
+        //         }));
+        //         return new Climate.RTree.SubTree<>(children);
+        //     } else {
+        //         long l = Long.MAX_VALUE;
+        //         int i = -1;
+        //         List<Climate.RTree.SubTree<T>> list = null;
+        //
+        //         for (int i1 = 0; i1 < paramSpaceSize; i1++) {
+        //             sort(children, paramSpaceSize, i1, false);
+        //             List<Climate.RTree.SubTree<T>> list1 = bucketize(children);
+        //             long l1 = 0L;
+        //
+        //             for (Climate.RTree.SubTree<T> subTree : list1) {
+        //                 l1 += cost(subTree.parameterSpace);
+        //             }
+        //
+        //             if (l > l1) {
+        //                 l = l1;
+        //                 i = i1;
+        //                 list = list1;
+        //             }
+        //         }
+        //
+        //         sort(list, paramSpaceSize, i, true);
+        //         return new Climate.RTree.SubTree<>(
+        //                 list.stream().map(subTree1 -> build(paramSpaceSize, Arrays.asList(subTree1.children)))
+        //                         .collect(Collectors.toList()));
+        //     }
+        // }
+        //
+        fn sort(children: []Node, comptime size: usize,
+                 comptime absolute: bool) void {
+
+std.mem.sort(Node, children, {}, struct {
+    fn lessThan(_: void, a: Node, b: Node) bool {
+
+            const comparator = comparator(size, absolute);
+            for (int i = 1; i < pspace_size; i++) {
+                comparator = comparator.thenComparing(comparator((size + i) % paramSpaceSize, absolute));
+            }
+
+    }
+}.lessThan);
+        }
+
+        fn comparator(comptime size: usize, comptime absolute: bool) fn lessThan(_: void, a: Node, b: Node)bool {
+        return struct {fn lessThan(_: void, a: Node, b: Node) bool {
+        const a_val = blk: {
+                const l = (a.parameter_space[size].min + a.parameter_space[size].max) / 2L;
+            break :blk if (absolute) @abs(l) else l;
+        };
+        const b_val = blk: {
+                            const l = (b.parameter_space[size].min + b.parameter_space[size].max) / 2L;
+                        break :blk if (absolute) @abs(l) else l;
+
+        };
+        return a_val < b_val;
+    }}.lessThan;
+        }
+
+        fn bucketize(nodes: []const Node) []const Node {
+            List<Climate.RTree.SubTree<T>> list = Lists.newArrayList();
+            List<Climate.RTree.Node<T>> list1 = Lists.newArrayList();
+            const i = std.math.pow(usize, children_per_node, @floor(@log(nodes.len - 0.01) / @log(children_per_node)));
+
+
+            for (nodes) |node| {
+                list1.add(node);
+                if (list1.size() >= i) {
+                    list.add(new Climate.RTree.SubTree<>(list1));
+                    list1 = Lists.newArrayList();
+                }
+            }
+
+            if (!list1.isEmpty()) {
+                list.add(new Climate.RTree.SubTree<>(list1));
+            }
+
+            return list;
+        }
+
+        fn cost(parameters: []const ParameterInstance) u64 {
+            var res: u64 = 0;
+            for (parameters) |parameter| res += parameter.max - parameter.min;
+            return res;
+        }
+
+        fn buildParameterSpace(children: []const Node) Node {
+            var res: []ParameterInstance = @span(.{});
+            for (children) |child| for (res, child.parameter_space) |curr, pspace| curr.merge(pspace);
+            return res;
+        }
+
+        pub fn search(self: *@This(), targetPoint: ParameterInstance) T {
+            const res = self.root.search(targetPoint, self.last);
+            self.last = res;
+            return res;
+        }
+
+        const Node = struct {
+            parameter_space: ParameterSpace,
+            value: union(enum) {
+                children: *[children_per_node]Node,
+                value: T,
+            },
+            fn search(self: @This(), instance: ParameterInstance, last: ?T) T {
+                switch (self.value) {
+                    .children => |children| {
+                        var best_dist = if (last) |l| metric(l, instance) else std.math.maxInt(i64);
+                        var best = last;
+
+                        for (children) |node| {
+                            if (best_dist > metric(node, instance)) {
+                                const res = node.search(instance, best);
+                                const curr_dist = metric(res, instance);
+                                if (best_dist > curr_dist) {
+                                    best_dist = curr_dist;
+                                    best = res;
+                                }
+                            }
+                        }
+
+                        return best.?;
+                    },
+                    .value => |val| return val,
+                }
+            }
+        };
+    };
+}
